61a406153ad2e11c0be8550b9e470d0982708df4
Suman Sourabh <sumsourabh14@gmail.com>
2022-09-10 00:28:27 +0530

    create two btns in navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { faApple } from "@fortawesome/free-brands-svg-icons";
-import { faBagShopping, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBagShopping, faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
@@ -40,23 +40,37 @@ function Navbar() {
 
     return (
         <header className="fixed left-0 right-0 bg-stone-700">
-            <nav className="border-2 container px-4 py-2 text-white mx-auto flex justify-between">
-                <a onClick={handleClick} className="md:hidden">
+            <nav className="container px-4 py-2 text-white mx-auto flex justify-between items-center">
+                {!navUl ? (
+                    <button onClick={handleClick} className="md:hidden text-xl">
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                    ) : (
+                    <button onClick={handleClick} className="md:hidden text-xl">
+                        <FontAwesomeIcon icon={faXmark} />
+                    </button>
+                    )
+                }
+                
+                {/* <a onClick={handleClick} className="md:hidden">
                     <FontAwesomeIcon icon={faBars} />
                 </a>
-                <a>
-                    <FontAwesomeIcon icon={faApple} />
+                <a className="md:hidden">
+                    <FontAwesomeIcon icon={faXmark} />
+                </a> */}
+                <a href="https://www.apple.com/in/">
+                    <FontAwesomeIcon icon={faApple} className="text-xl" />
                 </a>
-                <ul className={(navUl ? "flex" : "hidden") + " flex-col md:flex md:flex-row md:gap-4 md:text-sm absolute top-5 left-0 md:static"}>
+                <ul className={(navUl ? "flex" : "hidden") + " flex-col md:flex md:flex-row md:gap-4 md:text-sm absolute top-10 left-0 px-10 right-0 md:static bg-stone-700"}>
                     {navElements.map(navEl => {
                         return (
-                            <li key={navEl.id}>
+                            <li key={navEl.id} className="py-3 border-b md:px-0 md:py-0 md:border-none">
                                 <a href={navEl.link}>{navEl.name}</a>
                             </li>
                         )
                     })}
                 </ul>
-                <a href="">
+                <a href="https://www.apple.com/in/shop/goto/bag">
                     <FontAwesomeIcon icon={faBagShopping} />
                 </a>
             </nav>
