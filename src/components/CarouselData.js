10f8b77ad1c1fea33ba8582402cc908abce176e8
Suman Sourabh <sumsourabh14@gmail.com>
2022-09-11 17:28:35 +0530

    edit carousel daisyui component

--- src/components/CarouselData.js
+++ src/components/CarouselData.js
@@ -12,25 +12,34 @@ import img10 from "../carousel-images/1250x703--xQzMbV.jpg";
 const CarouselData = [
     {
         id: 1,
+        slide: "slide1",
         img: img1,
         title: "Drama",
         subtitle: "3 Emmy Nominations Including Best Actress",
         link: "https://tv.apple.com/in/show/the-morning-show/umc.cmc.25tn3v8ku4b39tr6ccgb8nl6m?itscg=10000&itsct=atv-apl_hp-stream_now--220622",
+        prev: "#slide2",
+        next: "#slide2",
     },
     {
         id: 2,
+        slide: "slide2",
         img: img2,
         title: "Documentary",
         subtitle: "Unknown. Unexplained. Unbelievable. Until now.",
         link: "https://tv.apple.com/in/show/prehistoric-planet/umc.cmc.4lh4bmztauvkooqz400akxav?itscg=10000&itsct=atv-apl_hp-stream_now--220622",
+        prev: "#slide1",
+        next: "#slide1",
     },
-    {
-        id: 3,
-        img: img3,
-        title: "Drama",
-        subtitle: "The epic series comes to an end.",
-        link: "https://tv.apple.com/in/show/see/umc.cmc.3s4mgg2y7h95fks9gnc4pw13m?itscg=10000&itsct=atv-apl_hp-stream_now--220622",
-    },
+    // {
+    //     id: 3,
+    //     slide: "slide3",
+    //     img: img3,
+    //     title: "Drama",
+    //     subtitle: "The epic series comes to an end.",
+    //     link: "https://tv.apple.com/in/show/see/umc.cmc.3s4mgg2y7h95fks9gnc4pw13m?itscg=10000&itsct=atv-apl_hp-stream_now--220622",
+    //     prev: "#slide2",
+    //     next: "#slide1",
+    // },
 ];
 
 export default CarouselData;
